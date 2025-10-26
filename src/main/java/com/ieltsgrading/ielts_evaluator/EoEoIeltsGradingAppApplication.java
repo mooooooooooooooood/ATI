2cf8ce9cd6dd75abe62287edf7a7ef307c69ba24
Yen Khanh <ngoc@gmail.com>
2025-10-27 00:22:41 +0700

    - Spring boot core

---
+++ src/main/java/com/ieltsgrading/ielts_evaluator/EoEoIeltsGradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ieltsgrading.ielts_evaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoEoIeltsGradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoEoIeltsGradingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ieltsgrading/ielts_evaluator/EoEoIeltsGradingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieltsgrading.ielts_evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoEoIeltsGradingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
